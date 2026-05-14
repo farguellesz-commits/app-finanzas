05e94bbbfcdecc1c2bc09158eb66c9d0cafe4c02
farguellesz-commits <farguellesz@gmail.com>
2026-05-14 19:03:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finanzas-v25'; // Solo tendrás que cambiar este numerito en el futuro
+const CACHE_NAME = 'finanzas-v26'; // Solo tendrás que cambiar este numerito en el futuro
 
 // INSTALACIÓN: Obligamos a instalar la nueva versión sin esperar
 self.addEventListener('install', event => {
